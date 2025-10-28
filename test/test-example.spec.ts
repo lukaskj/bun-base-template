b0f9d2db9beb5dfa12bc704d9153ec5fda1b7a26
Lukas K Jacomin <lukasjacomin@gmail.com>
2025-10-28 15:54:44 -0300

    fix: bun test todo mandatory callback

--- test/test-example.spec.ts
+++ test/test-example.spec.ts
@@ -1,7 +1,7 @@
 import { describe, expect, it } from "bun:test";
 
 describe("test example", () => {
-  it.todo("write more tests");
+  it.todo("write more tests", () => {});
 
   it("should pass", () => {
     expect(true).toBeTruthy();
